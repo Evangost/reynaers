a6feb8eec2430e6dabfc16d49b67fb648bdde0dc
Evangost <rekato1990@gmail.com>
2022-02-22 17:29:12 +0200

    progress 30%

--- src/js/index.js
+++ src/js/index.js
@@ -47,11 +47,11 @@ $(function () {
     }
 
     if ($('.swiper2').length) {
-        let slider2;
-        let slide2 = document.querySelectorAll('.swiper2 .swiper-slide').length;
+        let slider;
+        let slide = document.querySelectorAll('.swiper2 .swiper-slide').length;
 
-        if (slide2 > 1) {
-            slider2 = new Swiper('.swiper2', {
+        if (slide > 1) {
+            slider = new Swiper('.swiper2', {
                 observer: true,
                 observeParents: true,
                 loop: false,
@@ -68,11 +68,11 @@ $(function () {
     }
 
     if ($('.swiper3').length) {
-        let slider3;
-        let slide3 = document.querySelectorAll('.swiper3 .swiper-slide').length;
+        let slider;
+        let slide = document.querySelectorAll('.swiper3 .swiper-slide').length;
 
-        if (slide3 > 1) {
-            slider3 = new Swiper('.swiper3', {
+        if (slide > 1) {
+            slider = new Swiper('.swiper3', {
                 observer: true,
                 observeParents: true,
                 loop: false,
@@ -88,58 +88,32 @@ $(function () {
         }
     }
 
-    //Swiper slider secondary
+   /* if ($('.swiper-windows').length) {
+        let slider;
+        let slide = document.querySelectorAll('.swiper-windows .swiper-slide').length;
 
-
-    // Range slide
-   /* if ($('input[type="range"]')) {
-        let sliderRange = document.querySelectorAll('.slider-range');
-        let sliderHandles = document.querySelectorAll('.slider-handles');
-
-        if (sliderRange.length) {
-            sliderRange.forEach(function (elem) {
-                let input = elem.childNodes[0];
-                let startValue = input.hasAttribute('value') ? Number(input.getAttribute('value')) : 1;
-                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
-                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
-
-                input.remove();
-
-                noUiSlider.create(elem, {
-                    start: [startValue],
-                    step: 1,
-                    behavior: 'tap',
-                    connect: [true, false],
-                    range: {
-                        'min': [minValue],
-                        'max': [maxValue]
-                    }
-                });
+        if (slide > 1) {
+            slider = new Swiper('.swiper-windows', {
+                observer: true,
+                observeParents: true,
+                slidesPerView: 1,
+                navigation: {
+                    nextEl: '.swiper4-button-next',
+                    prevEl: '.swiper4-button-prev'
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                    type: 'fraction',
+                },
+                /!*scrollbar: {
+                    el: '.swiper-scrollbar',
+                },*!/
+                dynamicBullets: true,
             });
         }
 
-        if (sliderHandles.length) {
-            sliderHandles.forEach(function (elem) {
-                let input = elem.childNodes[0];
-                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
-                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
-
-                input.remove();
-
-                noUiSlider.create(elem, {
-                    start: [minValue, maxValue/2],
-                    step: 1,
-                    behavior: 'tap-drag',
-                    connect: true,
-                    range: {
-                        'min': minValue,
-                        'max': maxValue
-                    }
-                });
-            });
-        }
-    }
-*/
+    }*/
     // Lazy load observer
     const imagesAll = document.querySelectorAll('img[data-src]');
     let imgObserve = new IntersectionObserver(function (entries) {
